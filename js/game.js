21c3f374fe06c23193012db1226729a92e5cf8fd
jorgevalerio <jorgevalerio921@gmail.com>
2017-06-06 00:25:33 -0700

    Ready for PR
    Project works and i dont know why. :)

--- js/game.js
+++ js/game.js
@@ -1,282 +1,277 @@
 
- 
-let gameBoard = document.getElementById('board');
-gameBoard.style.display = "none";
-
-let body = document.body;
-
-let startScreen = document.createElement("div");
-startScreen.innerHTML = '<div class="screen screen-start" id="start"><header> <h1>Tic Tac Toe</h1><a href="#" class="button">Start game </a> </header></div>';
-body.appendChild(startScreen);
-
-//let startButton = document.getElementById('start');
-let startButton = document.getElementsByClassName('button');
-// hide 
-gameBoard.style.display = "none";
-
-
-startButton[0].addEventListener("click", function(){
-	startScreen.style.display = 'none';
-	gameBoard.style.display = 'block';
-});
-
-let boxArray =  document.getElementsByClassName('box');
-console.log(boxArray);
-
-// Other screnns
-
-function winnerScreenDiv(whow) {
-gameBoard.style.display = "none";
-let winnerScreen = document.createElement("div");
-winnerScreen.innerHTML = '<div class="screen screen-win ' + whow +'" id="finish"><header> <h1>Tic Tac Toe</h1><p class="message"></p><a href="#" class="button">New game</a></header></div>';
-body.appendChild(winnerScreen);
-
-	
-}
+(function(){
 
 
 
+	let tres;
+	let gameBoard = document.getElementById('board');
+	gameBoard.style.display = "none";
+	let body = document.body;
+	let startScreen = document.createElement("div");
+	startScreen.innerHTML = '<div class="screen screen-start" id="start"><header> <h1>Tic Tac Toe</h1><a href="#" class="button">Start game </a> </header></div>';
+	body.appendChild(startScreen);
+	let startButton = document.getElementsByClassName('button');
+	let boxArray =  document.getElementsByClassName('box');
+	let checkForWin = [];
+	let playsFor0 = [];
+	let playsForX = [];
+	let player0 = true;
+	let player0Div = document.getElementById('player1');
+	let playerX = false;
+	let playerXDiv = document.getElementById('player2');
+	let stopper = false;
 
-/*let winner0Screen = document.createElement("div");
-winner0Screen.innerHTML = '<div class="screen screen-win screen-win-one" id="finish"><header> <h1>Tic Tac Toe</h1><p class="message"></p><a href="#" class="button">New game</a></header></div>';
-body.appendChild(winner0Screen);
-winner0Screen.style.display = 'none';
+	// Possible wins
+	let win1 = '012';
+	let win2 = '345';
+	let win3 = '678';
+	let win4 = '036';
+	let win5 = '147';
+	let win6 = '258';
+	let win7 = '048';
+	let win8 = '246';
 
-
-let winnerXScreen = document.createElement("div");
-winnerXScreen .innerHTML = '<div class="screen screen-win screen-win-two" id="finish"><header> <h1>Tic Tac Toe</h1><p class="message"></p><a href="#" class="button">New game</a></header></div>';
-body.appendChild(winnerXScreen);
-winnerXScreen.style.display = 'none';
-
-
-let winner0Screen = document.createElement("div");
-startScreen.innerHTML = '<div class="screen screen-win screen-win-tie" id="finish"><header> <h1>Tic Tac Toe</h1><p class="message"></p><a href="#" class="button">New game</a></header></div>';
-body.appendChild(winner0Screen);
-winner0Screen.style.display = 'none';*/
-
-
-//Array to letters
-
-let checkForWin = [];
-let playsFor0 = [];
-let playsForX = [];
-
-let win1 = '012';
-let win2 = '345';
-let win3 = '678';
-let win4 = '036';
-let win5 = '147';
-let win6 = '258';
-let win7 = '048';
-let win8 = '246';
-
-// creating palyers
-
-let player0 = true;
-let player0Div = document.getElementById('player1');
-let playerX = false;
-let playerXDiv = document.getElementById('player2');
+	// this Event listener hides the welcome screen and starts the game
+	startButton[0].addEventListener("click", function(){
+		startScreen.style.display = 'none';
+		gameBoard.style.display = 'block';
+	});
 
 
-if (player0 === true){
-	
-	player0Div.classList.add('active');
-} 
+	// Functions
 
 
-if (playerX === true){
-	playerXDiv.classList.add('active');
-}
-
-function hasClass(el, cls) {
-  return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className);
-}
+	function winnerScreenDiv(whow) {
+		gameBoard.style.display = "none";
+		let winnerScreen = document.createElement("div");
+		winnerScreen.innerHTML = '<div class="screen screen-win ' + whow +'" id="finish"><header> <h1>Tic Tac Toe</h1><p class="message">Winner</p><a href="#" class="button">New game</a></header></div>';
+		body.appendChild(winnerScreen);
 
 
 
+		startButton[1].addEventListener("click", function(){
+			let finishDiv = document.getElementById('finish');
+			finishDiv.style.display = 'none';
+			console.log('s');
+			startScreen.style.display = 'none';
+			gameBoard.style.display = 'block';
+			playsFor0 = [];
+			playsForX = [];
 
-		function isWinnerCheck(winNumber, whoWin) {
-  		let a= whoWin[0];
+
+
+			finishDiv.innerHTML = '';
+			player0 = true;
+			player0Div.classList.add('active');
+			playerX = false;
+			playerXDiv.classList.remove('active');
+			stopper = false;
+			body.removeChild(winnerScreen);
+
+			for (iofArrayBox = 0; iofArrayBox < boxArray.length; iofArrayBox++){
+			boxArray[iofArrayBox].classList.remove("box-filled-1");
+			boxArray[iofArrayBox].classList.remove("box-filled-2");
+
+		}
+
+		});
+	}
+
+	// this function checks if the class exist and it it does it return true
+	function hasClass(el, cls) {
+	  return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className);
+	}
+
+
+	if (player0 === true){
+		player0Div.classList.add('active');
+	} else{
+		playerXDiv.classList.add('active');
+	}
+
+
+	// this function checks plays conbination for each player 
+
+	function isWinnerCheck(winNumber, whoWin, winScreen) {
+		let a= whoWin[0];
 		let b= whoWin[1];
 		let c= whoWin[2];
 		let d= whoWin[3];
+		let e= whoWin[4];
 		let towin3 = 0;
-			
-			
-			
-			if( winNumber.includes(a) === true){
-				towin3+=1;
-			}
-						if( winNumber.includes(b) === true){
-				towin3+=1;
-							console.log(towin3);
-			}
-						if( winNumber.includes(c) === true){
-				towin3+=1;console.log(towin3);
-			}
-						if( winNumber.includes(d) === true){
-				towin3+=1;console.log(towin3);
-			}
-						
-						if (towin3 >= 3){
-							console.log('ultimate winner');
-							let towin3 = 0;
-							console.log(towin3);
-							
-							
-							console.log('winner winner winner');
-						let winner12 = 'screen-win-one';
-						winnerScreenDiv(winner12);
-							
-						}
-		}
+
+		if( winNumber.includes(a) === true){
+					towin3+=1;
+			console.log('includes a')
+				}
+		if( winNumber.includes(b) === true){
+					towin3+=1;		
+			console.log('includes b')
+				}
+		if( winNumber.includes(c) === true){
+					towin3+=1;
+			console.log('includes c')
+				}
+		if( winNumber.includes(d) === true){
+					towin3+=1;
+			console.log('includes d')
+				}
+		if( winNumber.includes(e) === true){
+					towin3+=1;
+			console.log('includes e')
+				}
+
+		if (towin3 >= 3){
+			stopper = true;
+			let towin3 = 0;
+			let winner12 = winScreen;
+			winnerScreenDiv(winner12);
 
 
 
-for (iofArrayBox = 0; iofArrayBox < boxArray.length; iofArrayBox++){
-	
-	boxArray[iofArrayBox].addEventListener("mouseover", function(){
-		
-	if (player0 === true && hasClass(this, "box-filled-1") === false && hasClass(this, "box-filled-2") === false ) {
-		this.style.backgroundImage = "url('img/o.svg')";
-	} else if (playerX === true && hasClass(this, "box-filled-1") === false && hasClass(this, "box-filled-2") === false){
-		this.style.backgroundImage = "url('img/x.svg')";
+				}
 	}
-		
-	});
-}
+
+
+	// this for loops adds a backgroung img on hover to each square on the board
+
+	for (iofArrayBox = 0; iofArrayBox < boxArray.length; iofArrayBox++){
+		boxArray[iofArrayBox].addEventListener("mouseover", function(){
+
+			if (player0 === true && hasClass(this, "box-filled-1") === false && hasClass(this, "box-filled-2") === false ) {
+				this.style.backgroundImage = "url('img/o.svg')";
+			} else if (playerX === true && hasClass(this, "box-filled-1") === false && hasClass(this, "box-filled-2") === false){
+				this.style.backgroundImage = "url('img/x.svg')";
+				} else{}
+		});
+
+		boxArray[iofArrayBox].addEventListener("mouseout", function(){
+
+			this.style.backgroundImage = "";
+		});
+	}
 
 
 
+	for (iofArrayBox = 0; iofArrayBox < boxArray.length; iofArrayBox++){
+		let num =iofArrayBox;
 
 
-for (iofArrayBox = 0; iofArrayBox < boxArray.length; iofArrayBox++){
-	
-	boxArray[iofArrayBox].addEventListener("mouseout", function(){
-		
-		this.style.backgroundImage = "";
-		
-	});
+		boxArray[iofArrayBox].addEventListener("click", function(){
 
 
-}
-
-for (iofArrayBox = 0; iofArrayBox < boxArray.length; iofArrayBox++){
-	let num =iofArrayBox;
-	
-	boxArray[iofArrayBox].addEventListener("click", function(){
-		
-		
-		if (player0 === true && hasClass(this, "box-filled-1") === false && hasClass(this, "box-filled-2") === false) {
-			player0 = false;
-			playerX = true;
-			console.log('player 0 active');
-			playerXDiv.classList.add('active');
-			player0Div.classList.remove('active');
-			
-			console.log(num);
-			
-			if (hasClass(this, "box-filled-1") || hasClass(this, "box-filled-2")) {
-				
-				console.log(' has been selected already');
-			} else {
+		 if (player0 === true && hasClass(this, "box-filled-1") === false && hasClass(this, "box-filled-2") === false) {
+				player0 = false;
+				playerX = true;
+				playerXDiv.classList.add('active');
+				player0Div.classList.remove('active');
 				this.classList.add('box-filled-1');
 				playsFor0.push(num);
 				playsFor0.sort();
-				
 				let winner = playsFor0.join('');
-				
-				
-			
-				
-				
-				
+				let checkOneOnly = false;
+
 				if (playsFor0.length >=3){
-					
-					
-					console.log(winner);
-					isWinnerCheck(win1, playsFor0);
-					isWinnerCheck(win2, playsFor0);
-					isWinnerCheck(win3, playsFor0);
-					isWinnerCheck(win4, playsFor0);
-					isWinnerCheck(win5, playsFor0);
-					isWinnerCheck(win6, playsFor0);
-					isWinnerCheck(win7, playsFor0);
-					isWinnerCheck(win8, playsFor0);
-					
-					
-						
-					
+
+
+						isWinnerCheck(win1, playsFor0, 'screen-win-one');
+					if(stopper == true){return;}
+						isWinnerCheck(win2, playsFor0,'screen-win-one');
+					if(stopper == true){return;}
+						isWinnerCheck(win3, playsFor0, 'screen-win-one');
+					if(stopper == true){return;}
+						isWinnerCheck(win4, playsFor0, 'screen-win-one');
+					if(stopper == true){return;}
+						isWinnerCheck(win5, playsFor0, 'screen-win-one');
+					if(stopper == true){return;}
+						isWinnerCheck(win6, playsFor0, 'screen-win-one');
+					if(stopper == true){return;}
+						isWinnerCheck(win7, playsFor0, 'screen-win-one');
+					if(stopper == true){return;}
+						isWinnerCheck(win8, playsFor0, 'screen-win-one');
+					if(stopper == true){;return;}
 				}
-				
+			} 
+
+			if (playerX === true && hasClass(this, "box-filled-1") === false && hasClass(this, "box-filled-2")===false){
+				playerX = false;
+				player0 = true;
+				player0Div.classList.add('active');
+				playerXDiv.classList.remove('active');
+				//this.classList.add('box-filled-2');
+					if (hasClass(this, "box-filled-1") || hasClass(this, "box-filled-2")) {
+
+					} else {
+							this.classList.add('box-filled-2');
+							playsForX.push(num);
+							playsForX.sort();
+							let winner = playsForX.join('');
+
+								if (playsForX.length >=3){
+										isWinnerCheck(win1, playsForX,'screen-win-two');
+									if(stopper == true){return;}
+										isWinnerCheck(win2, playsForX,'screen-win-two');
+									if(stopper == true){return;}
+										isWinnerCheck(win3, playsForX,'screen-win-two');
+									if(stopper == true){return;}
+										isWinnerCheck(win4, playsForX,'screen-win-two');
+									if(stopper == true){return;}
+										isWinnerCheck(win5, playsForX,'screen-win-two');
+									if(stopper == true){;return;}
+										isWinnerCheck(win6, playsForX,'screen-win-two');
+									if(stopper == true){return;}
+										isWinnerCheck(win7, playsForX,'screen-win-two');
+									if(stopper == true){return;}
+										isWinnerCheck(win8, playsForX,'screen-win-two');
+									if(stopper == true){return;}
+									}			
+							}
+
+			} 
+
+			if (playsFor0.length === 5){
+					 console.log('last one was triggeres')
+					gameBoard.style.display = "none";
+					let winnerScreen = document.createElement("div");
+					winnerScreen.innerHTML = '<div class="screen screen-win screen-win-tie" id="finish"><header> <h1>Tic Tac Toe</h1><p class="message">It\'s a Draw</p><a href="#" class="button">New game</a></header></div>';
+					body.appendChild(winnerScreen);
+
+						for (iofArrayBox = 0; iofArrayBox < boxArray.length; iofArrayBox++){
+						boxArray[iofArrayBox].classList.remove("box-filled-1");
+						boxArray[iofArrayBox].classList.remove("box-filled-2");
+
+						}						
+
+
+						startButton[1].addEventListener("click", function(){
+
+
+
+						let finishDiv = document.getElementById('finish');
+						finishDiv.style.display = 'none';
+						console.log('s');
+						startScreen.style.display = 'none';
+						gameBoard.style.display = 'block';
+						playsFor0 = [];
+						playsForX = [];
+						winnerScreen.innerHTML = '';
+
+
+						finishDiv.innerHTML = '';
+						player0 = true;
+						player0Div.classList.add('active');
+						playerX = false;
+						playerXDiv.classList.remove('active');
+
+
+					});
 			}
-			
-		} 
-		
-		
-		
-		else if (playerX === true && hasClass(this, "box-filled-1") === false && hasClass(this, "box-filled-2")===false){
-			
-			playerX = false;
-			player0 = true;
-			console.log('player 1 active');
-			player0Div.classList.add('active');
-			playerXDiv.classList.remove('active');
-			//this.classList.add('box-filled-2');
-			
-				if (hasClass(this, "box-filled-1") || hasClass(this, "box-filled-2")) {
-				
-				console.log(' has been selected already');
-			} else {
-				this.classList.add('box-filled-2');
-				playsForX.push(num);
-				playsForX.sort();
-				
-				let winner = playsForX.join('');
-				
-				
-				if (playsForX.length >=3){
-					
-					
-					console.log(winner);
-					
-					
-					isWinnerCheck(win1, playsForX);
-					isWinnerCheck(win2, playsForX);
-					isWinnerCheck(win3, playsForX);
-					isWinnerCheck(win4, playsForX);
-					isWinnerCheck(win5, playsForX);
-					isWinnerCheck(win6, playsForX);
-					isWinnerCheck(win7, playsForX);
-					isWinnerCheck(win8, playsForX);
-					
-					
-				
-						/*let winner12 = 'screen-win-two';
-						winnerScreenDiv(winner12);*/
-					
-				}
-				
-			}
-			
-			
-			
-			
-			
-		} 
-		
-		
-		else{
-			
-		}
-		
-	});
 
 
-}
+			});
 
 
-// Creaing Arrays
+	}
 
-/*let arrayList = [];
-for(var i = 0; i < boxArray.length; i++) arrayList.push(boxArray[i]);*/
+	}());
+
